63e46a48d8adf2ef6265463664f3c7c418393deb
Rakesh Kanzariya. <rkanzariya1234@gmail.com>
2025-09-28 10:35:17 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://98.84.130.156";
+const API_BASE_URL = "http://98.84.130.156:84";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
